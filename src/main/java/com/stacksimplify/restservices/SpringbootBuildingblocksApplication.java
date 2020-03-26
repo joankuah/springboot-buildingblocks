3eb8ee5c2b8b9079f99576c58d7eb678627eab20
joankuah <joankuah@hotmail.com>
2020-03-26 17:41:51 +0800

    second commit

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -2,7 +2,7 @@ package com.stacksimplify.restservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// im testing if this comment gets committed
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
 
